f845df076e7ea771fadd933ecc3513daadc2bb0e
ichrisx7 <2003cjs@gmail.com>
2023-03-24 11:26:04 -0400

    Card

--- code/Poker/Form1.cs
+++ code/Poker/Form1.cs
@@ -12,7 +12,14 @@ namespace Poker {
     public partial class Form1 : Form {
         public Form1()
         {
+            Deck n = new Deck();
+            n.GetCard(0);
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
